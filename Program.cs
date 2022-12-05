d7b91636f44467e03a77cfcb053189508bc07b46
Bahadır Ş <bahadir102sahin@hotmail.com>
2022-12-05 22:19:55 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDeneme
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Test test = new Test();
+            test.Browser();
+            test.start();
+            test.search();
+        }
+
+        
+
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Firefox;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestDeneme
+{
+    class Test
+    {
+        IWebDriver driver;
+
+        public void Browser()
+        {
+            driver = new ChromeDriver();
+            
+        }
+
+        public void start()
+        {
+            string link = "https://www.network.com.tr/";
+            driver.Navigate().GoToUrl(link);
+            
+        }
+
+        public void search()
+        {
+
+            try
+            {
+                driver.FindElement(By.XPath("//*[@id='search']")).SendKeys("ceket"); //tag kısmının tam id'sini bulamadığım için bu şekilde bıraktım
+                Actions tus = new Actions(driver);
+                tus.SendKeys(Keys.Enter); //burada web sitesinde arama butonu olmadığı için aramayı yapmak için enter tuşuna basıyorum
+
+
+                Console.WriteLine("Element found by id");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            
+            
+        }
+
+    }
+}
